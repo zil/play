c3d2128d5d10c860c5d3dad70f360a77c40216e5
Vacker <fantanstic@gmail.com>
2017-07-24 19:08:25 +0800

    graph + test_data

--- src/main/java/fun/play/alog/graph/Graph.java
+++ src/main/java/fun/play/alog/graph/Graph.java
@@ -1,5 +1,6 @@
 package fun.play.alog.graph;
 
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Scanner;
 
@@ -7,6 +8,7 @@ import fun.play.alog.LinkedList;
 
 public class Graph {
 	private LinkedList<Integer>[] adj;
+	private int E = 0;
 	
 	public Graph(InputStream in) {
 		try(Scanner scanner = new Scanner(in)){
@@ -44,15 +46,31 @@ public class Graph {
 	
 	public void addEdge(int u,int v){
 		adj[u].add(v);
+		E++;
 	}
 	
 	public Iterable<Integer> adj(int v){
 		return adj[v];
 	}
 	
-	public static void main(String[] args) {
-		Graph graph = new Graph(System.in);
-		System.err.println("vertex from 0:" + graph.adj(0));
+	public int E() {
+		return this.E;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < adj.length; i++) {
+			if(adj[i].isEmpty())continue;
+			
+			sb.append(i).append(":" + adj[i]).append("\n");
+		}
+		return sb.toString();
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		Graph graph = new Graph(Graph.class.getResourceAsStream("mediumG.txt"));
+		System.err.println(graph);
 	}
 	
 }
